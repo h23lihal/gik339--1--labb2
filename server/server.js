1fdefac2ac59313586c90b09e8a05604017aab88
h23lihal <l.hellstrom95@gmail.com>
2024-12-11 11:17:31 +0100

    .

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const server = express();
+const sqlite3 = require('sqlite3').verbose();
 server
   .use(express.json())
-  .use(express.urlencoded({ extendet: false }))
+  .use(express.urlencoded({ extended: false }))
   .use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Acess-Control-Allow-Headers', '*');
@@ -13,4 +14,16 @@ server
 
 server.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
+});
+
+server.get('/users', (req, res) => {
+  const db = new sqlite3.Database('./gik339-labb2.db');
+  const sql = 'SELECT * FROM users';
+  db.all(sql, (err, rows) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(rows);
+    }
+  });
 });
