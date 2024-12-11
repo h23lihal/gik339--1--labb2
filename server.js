bf38785f0f89ebfecb56378e42d229a0bebc7022
h23lihal <l.hellstrom95@gmail.com>
2024-12-11 09:28:14 +0100

    .

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const server = express();
 server
-    .use(express.json())
-    .use(express.urlencoded({extendet: false}))
-    .use((req,res,next) =>{
-        res.header('Access-Control-Allow-Origin', '*'); 
-        res.header ('Acess-Control-Allow-Headers', '*'); 
-        res.header ('Access-Control-Allow-Methods', '*'); 
-        next();
-    });
+  .use(express.json())
+  .use(express.urlencoded({ extendet: false }))
+  .use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Acess-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+
+    next();
+  });
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
